3cff86ea5a93423bc0e8d804efeaa3d19ebf73b7
Marian Carnoky <mcarnoky001@cee.ema.ad.pwcinternal.com>
2018-10-14 15:01:46 +0200

    [TASK] first commit of project init with first files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  single: any[] = [
+    {
+      name: "Test1",
+      value: 8940000
+    },
+    {
+      name: "Test2",
+      value: 5000000
+    },
+    {
+      name: "Test3",
+      value: 7200000
+    }
+  ];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = "Test Values";
+  showYAxisLabel = true;
+  yAxisLabel = "Value";
+
+  colorScheme = {
+    domain: ["#5AA454", "#A10A28", "#C7B42C", "#AAAAAA"]
+  };
+
+  onSelect(event) {
+    console.log(event);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule, NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
